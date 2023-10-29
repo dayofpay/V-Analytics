063f53d13881144b57e35b40bfbc91429bd613a1
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-10-29 05:37:16 +0200

    Login logic implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,7 +11,21 @@ router.get('/login',middlewares.preventAuthenticatedUser, async (req, res) => {
     res.render('auth/login');
   });
 
+router.post('/login',middlewares.preventAuthenticatedUser,async (req,res) => {
 
+    const {email,password} = req.body;
+    const token = await userServices.login(email,password);
+
+    if(token.hasOwnProperty('error')){
+        console.log('Invalid username or password.');
+        res.render('auth/login',{hasError:true,errorMessage:token.error});
+
+        return;
+    }
+
+    res.cookie('auth',token,{httpOnly:true});
+    res.redirect('/');
+})
 router.get('/register',middlewares.preventAuthenticatedUser,async(req,res) => {
     res.render('auth/register');
 });
@@ -24,13 +38,23 @@ router.post('/register',middlewares.preventAuthenticatedUser,async(req,res) => {
     const token = await userServices.createUser({name,company,email,password,repeatPassword});
 
     if (token.hasOwnProperty('error')) {
-        console.log("Invalid username or password.");
+        console.log("Error has occured.");
         res.render('auth/register', {hasError:true, errorMessage: token.error });
         return;
     }
   
     console.log(token);
     res.cookie("auth", token, { httpOnly: true });
+    res.redirect("/");
+});
+
+
+
+
+
+
+router.get('/logout',middlewares.protectedRoute,async(req,res) => {
+    res.clearCookie("auth");
     res.redirect("/");
 })
   
