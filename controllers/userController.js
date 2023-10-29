e03bc1d276dfd2e1b23333b08aa8c34ba6923341
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-10-29 21:59:45 +0200

    User Services - Part II

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const User = require('../models/User');
 const middlewares = require('../middlewares/auth');
 
-router.post('/seen', middlewares.protectedRoute, async (req, res) => {
+router.get('/:id', middlewares.protectedRoute, async (req, res) => {
+
+    const userId = req.params.id;
+
+    res.render('profiles/index');
 });
 
 module.exports = router;
