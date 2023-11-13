ad8a8dac79c385477c51caa0d165e0e356a015f2
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-11-13 03:14:51 +0200

    Fixed cors issue

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -1,13 +1,14 @@
 const path = require('path');;
 const cookieParse = require("cookie-parser");
 const {auth}  = require('../middlewares/auth');
-
+const cors = require('cors');
 const expressConfig = (app,express) => {
     app.use(express.static(path.resolve(__dirname,'../public')));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParse());
     app.use(auth);
+    app.use(cors());
 }
 
 
